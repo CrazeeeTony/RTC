2ac0cc0994313f12570d5bf9ed72e7680b8dd23e
CrazeeeTony <weijia.tony.li@gmail.com>
2015-12-01 13:46:10 -0500

    Piece Update

---
+++ Piece.java
@@ -0,0 +1,19 @@
+public class Piece{
+	public static final int KING = 0;
+	public static final int QUEEN = 1;
+	public static final int BISHOP = 2;
+	public static final int KNIGHT = 3;
+	public static final int ROOK = 4;
+	public static final int PAWN = 5;
+	
+	int xPos;
+	int yPos;
+	int pieceID
+	long coolDown;
+	
+	public Piece(int xPos, int yPos, int pieceID){
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.pieceID = pieceID;
+	}
+}
