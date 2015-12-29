53143db26dd6d63dafbaa8987bc52c84a114a14b
Thefak Eperson <mrthefakeperson@gmail.com>
2015-12-28 19:57:31 -0500

    updated menu class
    it now looks slightly nicer (very subjective)

--- Menu.java
+++ Menu.java
@@ -4,17 +4,25 @@
  */
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.*;
 import javax.swing.*;
 import javax.imageio.ImageIO;
-import java.io.File;
+import java.io.*;
+import java.util.Scanner;
 public class Menu extends JFrame implements ActionListener
 {
+	//for line breaks in the html formatting used in JComponents
+	public static final String LINE_END = "<br>";
+	
 	DetectAction da;
-	JPanel everything = new JPanel();
-	JPanel graphicPanel = new JPanel();
-	JPanel buttonPanel = new JPanel();
-	JLabel title = new JLabel();
-	JLabel message = new JLabel();
+	JPanel everything = 	new JPanel();
+	JPanel information = 	new JPanel();
+	JPanel graphicPanel = 	new JPanel();
+	JPanel textPanel = 		new JPanel();
+	JPanel buttonPanel = 	new JPanel();
+	JLabel picture =		new JLabel();
+	JLabel title = 			new JLabel();
+	JLabel message = 		new JLabel();
 	JButton[] buttons = 
 	 {
 		new JButton("play game"),
@@ -29,10 +37,13 @@ public class Menu extends JFrame implements ActionListener
 	{
 		super("RTC");
 		
-		try{
+		try
+		{
 			this.setIconImage(ImageIO.read(new File("Piece Images/1Knight.png")));
 		}
-		catch(Exception e){
+		catch(Exception e)
+		{
+			System.out.println("Error: Images could not be opened.");
 		}
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -40,29 +51,51 @@ public class Menu extends JFrame implements ActionListener
 		
 		message.setText("This is the menu.");		//FUTURE: figure out a suitable initial message
 
-		//graphicPanel contains all components in the top half of screen (non-interactive)
-		graphicPanel.add(title);
-		graphicPanel.add(message);
-		graphicPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 99999, 5));
+		//textPanel contains all components in the top right quarter of screen (non-interactive)
+		textPanel.add(title);
+		textPanel.add(message);
+		textPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 99999, 5));
 		
 		for (JButton e : buttons)
 		{
 			e.addActionListener(this);
 			buttonPanel.add(e);
+			e.setPreferredSize(new Dimension(150, 35));
 		}//end for
 
-		//button panel contains the interactive components (JButtons)
+		//button panel contains the interactive components (JButtons) in the bottom right
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 99999, 5));
 
-		//add panels to background panel, set layout
+		//add panels to a background panel for message display and buttons, set layout
+		information.add(textPanel);
+		information.add(buttonPanel);
+		information.setLayout(new BoxLayout(information, BoxLayout.Y_AXIS));
+		information.setPreferredSize(new Dimension(300, 400));
+		
+		//the remaining left half of the screen is a nice looking image
+		try
+		{
+			BufferedImage retrievedPic = ImageIO.read(new File("title_screen.png"));
+			picture.setIcon(new ImageIcon(retrievedPic));
+			picture.setPreferredSize(new Dimension(300, 400));
+			graphicPanel.add(picture);
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error: could not load image file.");
+		}
+		graphicPanel.setPreferredSize(new Dimension(300, 400));
+		
+		//add both image and information to the background panel, set their layout to be side-by-side
 		everything.add(graphicPanel);
-		everything.add(buttonPanel);
-		everything.setLayout(new BoxLayout(everything, BoxLayout.Y_AXIS));
+		everything.add(information);
+		everything.setPreferredSize(new Dimension(600,400));
+		everything.setLayout(new BoxLayout(everything, BoxLayout.X_AXIS));
 		
 		//set up the frame
 		this.add(everything);
+		this.pack();
 		this.setVisible(true);
-		this.setSize(new Dimension(600,400));		//FUTURE: decide on more permanent dimensions
 	}//end default constructor ()
 	
 	/**
@@ -87,10 +120,26 @@ public class Menu extends JFrame implements ActionListener
 				break;
 			case 1:
 				//clicked "help"
-
+				message.setText("Click 'play game' to play the game.");
+				
 				break;
 			case 2:
 				//clicked "highscores"
+				try
+				{
+					message.setText("<html>highscores" + LINE_END);
+					Scanner reading = new Scanner(new File("highscores.txt"));
+					
+					while (reading.hasNext())
+					{
+						message.setText(message.getText() + LINE_END + reading.nextLine());
+					}
+					message.setText(message.getText() + "</html>");
+				}
+				catch (IOException e)
+				{
+					System.out.println("Error: could not read the highscores file.");
+				}
 				break;
 			case 3:
 				//clicked "quit"
