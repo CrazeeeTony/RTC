3fcf9cfc3ddc8ac9236fe789d6c2f26fd524a13a
Thefak Eperson <mrthefakeperson@gmail.com>
2016-01-14 20:28:54 -0500

    added a pause menu
    also changed detectAction a bit

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-javac Main.java Piece.java GameScreen.java Menu.java Coord.java AI.java GameOverScreen.java
+javac Main.java Piece.java GameScreen.java Menu.java Coord.java AI.java GameOverScreen.java OptionsWindow.java
 pause
 java Main
 pause
