2d6c7e8dce8fda1ae27cade102390ad4d4cdbeb5
CrazeeeTony <weijia.tony.li@gmail.com>
2016-01-18 09:00:22 -0500

    Jar

--- run.bat
+++ run.bat
@@ -1,5 +1,7 @@
 javac -version
 javac Main.java Piece.java GameScreen.java Menu.java Coord.java AI.java GameOverScreen.java Player.java
 pause
-java Main
+jar cfm RTC.jar Manifest.mf *.class
+pause
+java -jar RTC.jar
 pause
